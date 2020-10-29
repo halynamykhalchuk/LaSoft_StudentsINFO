d7d64373eed5fab089a42080e2b7f0fe60ab5a7d
Halyna <mykhalchukhalyna@gmail.com>
2020-10-29 19:15:14 +0200

    wrote templates for objects

--- app.js
+++ app.js
@@ -52,3 +52,48 @@ let group = {
     groupHead: student,
     groupMembers: []
 }
+
+
+//Hostel rooms
+let r1 = Object.create(room);
+    r1.nmbr = 421;
+    r1.ppl = 2;
+
+let r2 = Object.create(room);
+    r2.nmbr = 517;
+    r2.ppl = 3;
+
+
+
+//Subjects
+let sub1 = Object.create(subject);
+    sub1.subjName = 'Topology'
+    sub1.subjProf = 'Oleh Hutik'
+
+let sub2 = Object.create(subject);
+    sub2.subjName = 'Astronomy'
+    sub2.subjProf = 'Valentin Lavrik'
+
+
+
+//Books
+let b1 = Object.create(subject)
+    b1.num = 188501,
+    b1.title = 'Kobzar',
+    b1.author = 'Taras Shevchenko',
+    b1.genre = 'Poetry',
+    b1.price = 99
+
+let b2 = Object.create(subject)
+    b2.num = 103601,
+    b2.title = 'The Complex Numbers Study',
+    b2.author = 'Chyzhykov',
+    b2.genre = 'Math',
+    b2.price = 50
+
+let b3 = Object.create(subject)
+    b3.num = 84101,
+    b3.title = 'The Avengers',
+    b3.author = 'Sten Lee',
+    b3.genre = 'Comics',
+    b3.price = 25
